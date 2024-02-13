7dfdcc7692f2d41a114e45f1936eb7bbacf46dd8
Helsamet <helsamet001@gmail.com>
2024-02-13 14:31:04 +0700

    cvdmrr

---
+++ include/navbar.php
@@ -0,0 +1,25 @@
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="index.php" class="navbar-brand p-0">
+                    <h1 class="m-0">CVDMRR</h1>
+                  
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                        <a href="about.php" class="nav-item nav-link">About</a>
+                        <a href="service.php" class="nav-item nav-link">Service</a>
+                        <a href="team.php" class="nav-item nav-link">Our Team</a>
+                        <a href="contact.php" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <a href="quote.php" class="btn btn-light rounded-pill text-primary py-2 px-4 ms-lg-5">Free Quote</a>
+                </div>
+            </nav>
+
+        </div>
+        <!-- Navbar & Hero End -->

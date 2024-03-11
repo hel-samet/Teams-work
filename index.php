59438cf05bc6e2a8275f8cdd3c0d83990c0a0c16
Helsamet <helsamet001@gmail.com>
2024-03-11 15:01:48 +0700

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="SEO គឺជាដំណើរការនៃការបង្កើនភាពមើលឃើញឬងាយស្រួលក្នុងការស្វែងរកនិងការចាត់វិធានការដើម្បីជួយឱ្យគេហទំព័រឬផ្នែកមាតិកាមានចំណាត់ថ្នាក់ខ្ពស់នៅក្នុងលទ្ធផលស្វែងរករបស់ Google។ ">
     <meta name="author" content="Hel samet">
     
-    <meta property="og:image" content="https://media.sabay.com/media/publications/logos/61d5046055fd2_1641350220.png" />
+    <meta property="og:image" content="https://media.cvdmrr.com/media/publications/logos/61d5046055fd2_1641350220.png" />
     <meta property="og:url" content="https://cvdmrr.helsamet.com" />
     <meta property="og:title" content="cvdmrr" />
     <meta property="og:description" content="cvdmrr proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!" />

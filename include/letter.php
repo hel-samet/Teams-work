1f6d7037aa9925acbf52c27dfefcccd6529be9b5
Helsamet <helsamet001@gmail.com>
2024-03-12 15:36:39 +0700

    update

--- include/letter.php
+++ include/letter.php
@@ -7,7 +7,7 @@
                         <small  style="font-family: 'battambang', cursive;  font-weight: 300;" class="text-white">សូមធ្វើការទាក់ទងមកខ្ញុំតាមរយះ</small>
                         <div class="position-relative w-100 mt-3">
                             <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="text" placeholder="Enter Your Email" style="height: 48px;">
-                           <a href="https://t.me/helsamet" target="_blank"> <button  type="button" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-secondary fs-4"></i></button></a>
+                           <a href="https://t.me/helsamet"target="_blank" rel="noopener noreferrer"> <button  type="button" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-secondary fs-4"></i></button></a>
                         </div>
                     </div>
                     <div class="col-md-6 text-center mb-n5 d-none d-md-block">

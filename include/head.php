a286ea594b57423abc85d49b129bdc795cdba188
Helsamet <helsamet001@gmail.com>
2024-03-12 15:51:15 +0700

    update

---
+++ include/head.php
@@ -0,0 +1,38 @@
+<head>
+    <title>CVDMRR_TEAM2_PROJECT</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="keywords" content="breaking news, entertainment, technology, life, sport, cambodia">
+    <meta name="description" content="SEO Plans to build strong or limited website or content restrictions in search results Google។ ">
+    <meta property="og:image" content="https://media.cvdmrr.com/media/publications/logos/61d5046055fd2_1641350220.png" />
+    <meta property="og:url" content="https://cvdmrr.helsamet.com" />
+    <meta property="og:title" content="cvdmrr" />
+    <meta property="og:description" content="cvdmrr proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!" />
+    <meta property="og:type" content="website" />
+    <meta property="fb:pages" content="1056231387765931"/><meta property="fb:app_id" content="745303362249043"/><meta property="fb:admins" content=""/>
+    <meta name="twitter:card" content="summary_large_image"><meta name="twitter:site" content="@cvdmrr"><meta name="twitter:creator" content="@cvdmrr">
+    <meta name="twitter:title" content="cvdmrr">
+    <meta name="twitter:description" content="cdmrr proudly is the most visited and popular Khmer website, with more than 20 million page views and almost 2 millions unique visitors per month!">
+    <meta name="twitter:image" content="https://media.cvdmrr.com/media/publications/logos/61d5046055fd2_1641350220.png">
+
+    <!-- Favicon -->
+    <link href="img/icon.png" rel="icon" alt="CVDMRRS">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
